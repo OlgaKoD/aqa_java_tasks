842ea1ab4812d196a5557c0346798d6feb10f646
OlgaKoD <60149491+OlgaKoD@users.noreply.github.com>
2020-01-30 23:35:39 +0200

    Update Task5. java

--- src/main/java/lesson1/Task5.java
+++ src/main/java/lesson1/Task5.java
@@ -7,9 +7,14 @@ package lesson1;
 public class Task5 {
     public static void main(String[] args) {
         //System.out.println(reverse("Hello world!!!"));
+        System.out.println(reverse("Hello world!!!"));
     }
 
-    public static String reverse(String str) {
-        return "";
+ 	public static String reverse(String str) {
+   	String result = ""; 
+   	char[] Hi = str.toCharArray(); 
+   	for (int i = Hi.length-1; i>=0; i--) {
+    result =  result + Hi[i];}
+    return result;
     }
 }
