079eec317ab388fdd0b1c92c5d1500e1757fc2e6
OlgaKoD <60149491+OlgaKoD@users.noreply.github.com>
2020-02-01 13:33:54 +0200

    Update Task3.java

--- src/main/java/lesson1/Task3.java
+++ src/main/java/lesson1/Task3.java
@@ -1,29 +1,16 @@
 package lesson1;
-
 /* Мое имя
 Вывести на экран свое имя 5 строк по 10 раз (через пробел).
 System.out.println, System.out.print можно использовать только по одному разу
 для этого нужно воспользоваться циклом while
 */
-
-public class Task3 {
-
-    public static void main(String[] args) {
-        String s = "Vladimir";
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-    }
+public class Task3 { 
+public static void main(String[] args) { 
+String s = "Olga"; 
+int i = 0;
+while (i<5){
+System.out.println( s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s);
+i++;
+}
+}
 }
